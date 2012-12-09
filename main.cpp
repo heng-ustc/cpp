4c9bbf96deb0ff7d4ba0e44d22ca0aeda5ab168e
heng-ustc <hengliu@outlook.com>
2012-12-09 19:46:18 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,11 +8,12 @@ int main(int argc, char** argv)
 
     for(unsigned i = 0; i < vec.size(); ++i)
     {
-        vec[i] = i;
+        vec.at[i] = i;
     }
 
     cout<<"vec[10] = "<<vec[10]<<endl;
     cout<<"size of vec is: "<<vec.size()<<endl;
+    
     vector<int>::iterator it = vec.begin() + 10;
     vec.erase(it);
     cout<<"vec[10] = "<<vec[10]<<endl;
