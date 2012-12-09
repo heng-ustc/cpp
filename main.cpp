81ff155ffa186b8e11a110f18e3b032292dd2ae2
Heng <hengliu@outlook.com>
2012-12-09 19:44:25 +0800

    change to vector

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-#include
+#include <vector>
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-    cout << "Hello world!" << endl;
+    vector<int> vec(100);
+
+    for(unsigned i = 0; i < vec.size(); ++i)
+    {
+        vec[i] = i;
+    }
+
+    cout<<"vec[10] = "<<vec[10]<<endl;
+    cout<<"size of vec is: "<<vec.size()<<endl;
+    vector<int>::iterator it = vec.begin() + 10;
+    vec.erase(it);
+    cout<<"vec[10] = "<<vec[10]<<endl;
+    cout<<"size of vec is: "<<vec.size()<<endl;
+
     return 0;
 }
