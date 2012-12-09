df574f7697fd9400d9c7d81dff61193d3558c2c4
Heng <hengliu@outlook.com>
2012-12-09 19:51:19 +0800

    change 10 to 11

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@ int main(int argc, char** argv)
     cout<<"vec[10] = "<<vec[10]<<endl;
     cout<<"size of vec is: "<<vec.size()<<endl;
     
-    vector<int>::iterator it = vec.begin() + 10;
+    vector<int>::iterator it = vec.begin() + 11;
     vec.erase(it);
-    cout<<"vec[10] = "<<vec[10]<<endl;
+    cout<<"vec[11] = "<<vec[11]<<endl;
     cout<<"size of vec is: "<<vec.size()<<endl;
 
     return 0;
