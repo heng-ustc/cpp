b3fd4b9218fed56b7a216a1ccbc3d395b09ec9a8
Heng <hengliu@outlook.com>
2012-12-13 17:18:53 +0800

    change bitset.cpp

--- bitset.cpp
+++ bitset.cpp
@@ -2,30 +2,49 @@
 #include <iostream>
 #include <string>
 #include <bitset>
+
 using namespace std;
 
 int main ()
 {
-  bitset<4> first (string("1001"));
-  bitset<4> second (string("0011"));
+    unsigned char b1 = 0x12;
+    unsigned char b2 = 0x32;
+    unsigned char res = b1 | b2;
+    cout<<bitset<8>(b1)<<endl;
+    cout<<bitset<8>(b2)<<endl;
+    cout<<bitset<8>(res)<<endl;
+    cout<<true<<' '<<false<<endl;
+    cout<<boolalpha
+        <<true<<'\t'<<false<<endl;
+    cout<<noboolalpha
+        <<true<<'\t'<<false<<endl;
 
-  cout << (first^=second) << endl;          // 1010 (XOR,assign)
-  cout << (first&=second) << endl;          // 0010 (AND,assign)
-  cout << (first|=second) << endl;          // 0011 (OR,assign)
+    cout<<sizeof(char)<<' '<<sizeof(int)<<' '
+        <<sizeof(float)<<' '<<sizeof(signed)<<' '
+        <<sizeof(double)<<' '<<sizeof(vector)<<' ';
 
-  cout << (first<<=2) << endl;              // 1100 (SHL,assign)
-  cout << (first>>=1) << endl;              // 0110 (SHR,assign)
-
-  cout << (~second) << endl;                // 1100 (NOT)
-  cout << (second<<1) << endl;              // 0110 (SHL)
-  cout << (second>>1) << endl;              // 0001 (SHR)
-
-  cout << (first==second) << endl;          // false (0110==0011)
-  cout << (first!=second) << endl;          // true  (0110!=0011)
-
-  cout << (first&second) << endl;           // 0010
-  cout << (first|second) << endl;           // 0111
-  cout << (first^second) << endl;           // 0101
+//  bitset<4> first  (string("1001"));
+//  bitset<4> second (string("0011"));        // second keeps 0011.
+//
+//  cout << (first^=second) << endl;          // 1010 (XOR,assign)
+//  cout << (first&=second) << endl;          // 0010 (AND,assign)
+//  cout << (first|=second) << endl;          // 0011 (OR,assign)
+//
+//  cout << (first<<=2) << endl;              // 1100 (SHL,assign)
+//  cout << (first>>=1) << endl;              // 0110 (SHR,assign), first = 0110 now
+//
+//  cout << (~second) << endl;                // 1100 (NOT)
+//  cout << (second<<1) << endl;              // 0110 (SHL)
+//  cout << (second>>1) << endl;              // 0001 (SHR)
+//
+//  cout << (first==second) << endl;          // false (0110==0011)
+//  cout << (first!=second) << endl;          // true  (0110!=0011)
+//
+////   cout<<first<<second<<endl;
+//
+//  cout << (first&second) << endl;           // 0010
+//  cout << (first|second) << endl;           // 0111
+//  cout << (first^second) << endl;           // 0101
 
   return 0;
 }
